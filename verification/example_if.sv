809ac3ddfb89249ba344ff8a35d507404525ba96
Kyle O'Brien <kyle@keelfw.com>
2025-09-19 14:32:40 -0400

    Fix property assertions

--- verification/example_if.sv
+++ verification/example_if.sv
@@ -16,15 +16,19 @@ interface example_if #(
 
     property R_Scratch_Register_O_upd_pulse;
         @(posedge aclk) disable iff (!aresetn)
-        (axi4_lite.awvalid && axi4_lite.awready && axi4_lite.awaddr == 4) |->
-        ##[1:$] (axi4_lite.wready && axi4_lite.wvalid) ##2 R_Scratch_Register_O_upd ##1 !R_Scratch_Register_O_upd;
+        (axi4_lite.awvalid && axi4_lite.awready && (axi4_lite.awaddr == 4)) |->
+        ##[1:25] (axi4_lite.wready && axi4_lite.wvalid)
+        ##1 (R_Scratch_Register_O_upd && !$past(R_Scratch_Register_O_upd))
+        ##1 (!R_Scratch_Register_O_upd);
     endproperty
     assert property (R_Scratch_Register_O_upd_pulse);
 
     property R_Register_with_Fields_O_upd_pulse;
         @(posedge aclk) disable iff (!aresetn)
-        (axi4_lite.awvalid && axi4_lite.awready && axi4_lite.awaddr == 64) |->
-        ##[1:$] (axi4_lite.wready && axi4_lite.wvalid) ##2 R_Register_with_Fields_O_upd ##1 !R_Register_with_Fields_O_upd;
+        (axi4_lite.awvalid && axi4_lite.awready && (axi4_lite.awaddr == 64)) |->
+        ##[1:25] (axi4_lite.wready && axi4_lite.wvalid)
+        ##1 (R_Register_with_Fields_O_upd && !$past(R_Register_with_Fields_O_upd))
+        ##1 (!R_Register_with_Fields_O_upd);
     endproperty
     assert property (R_Register_with_Fields_O_upd_pulse);
 
