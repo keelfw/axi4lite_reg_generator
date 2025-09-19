120ad4005ec2ea847cf48e77b807429b0cd68a82
Kyle O'Brien <kyle@keelfw.com>
2025-09-19 10:12:26 -0400

    Added properties to example_if to check upd pulse

--- verification/example_if.sv
+++ verification/example_if.sv
@@ -12,4 +12,18 @@ interface example_if #(
     logic [14:0] R_Register_with_Fields_O;
     logic R_Register_with_Fields_O_upd;
 
+    property R_Scratch_Register_O_upd_pulse;
+        @(posedge aclk) disable iff (!aresetn)
+        (axi4_lite.awvalid && axi4_lite.awready && axi4_lite.awaddr == 4) |->
+        ##[1:$] (axi4_lite.wready && axi4_lite.wvalid) ##2 R_Scratch_Register_O_upd ##1 !R_Scratch_Register_O_upd;
+    endproperty
+    assert property (R_Scratch_Register_O_upd_pulse);
+
+    property R_Register_with_Fields_O_upd_pulse;
+        @(posedge aclk) disable iff (!aresetn)
+        (axi4_lite.awvalid && axi4_lite.awready && axi4_lite.awaddr == 64) |->
+        ##[1:$] (axi4_lite.wready && axi4_lite.wvalid) ##2 R_Register_with_Fields_O_upd ##1 !R_Register_with_Fields_O_upd;
+    endproperty
+    assert property (R_Register_with_Fields_O_upd_pulse);
+
 endinterface : example_if
